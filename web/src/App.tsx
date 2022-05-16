05680c542c614698630d2144d00a22624759a7e6
Pulkit Sood <soodpulkit21@gmail.com>
2022-05-16 08:44:56 +0530

    fix(auth): :bug: fix sanctum configuration

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Sanctum } from "react-sanctum";
 import axios from "axios";
+// https://laravel.com/docs/9.x/sanctum#cors-and-cookies
+axios.defaults.withCredentials = true;
 const sanctumConfig = {
   // Your application URL
-  apiUrl: "http://localhost:8080",
+  apiUrl: "http://localhost:80",
   // The following settings are URLS that need to be created in your Laravel application
   // The URL sanctum uses for the csrf cookie
   csrfCookieRoute: "sanctum/csrf-cookie",
   // {email: string, password: string, remember: true | null} get POSTed to here
-  signInRoute: "api/login",
+  signInRoute: "login",
   // A POST request is sent to this route to sign the user out
-  signOutRoute: "api/logout",
+  signOutRoute: "logout",
   // Used (GET) for checking if the user is signed in (so this should be protected)
   // The returned object will be avaiable as `user` in the React components.
-  userObjectRoute: "api/user",
+  userObjectRoute: "api/user/profile",
   // The URL where the OTAP token or recovery code will be sent to (optional).
   // Only needed if you want to use two factor authentication.
   twoFactorChallengeRoute: "two-factor-challenge",
